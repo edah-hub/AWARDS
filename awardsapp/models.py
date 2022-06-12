693f5823586d2327f62861074446bc3735516a95
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-13 00:36:44 +0300

    adds templates

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -87,6 +87,43 @@ class Project(models.Model):
         return self.title
     
     
+class Profile(models.Model):
+    class Meta:
+        db_table = 'profile'
+
+    bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
+    profile_pic = models.ImageField(upload_to='picture/', null=True, blank=True, default= 0)
+    user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name="profile")
+    project=models.ForeignKey(Project, on_delete=models.CASCADE,null=True)
+    contact=models.IntegerField(default=0)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    post_save.connect(create_user_profile, sender=User)
+
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+
+    @classmethod
+    def search_users(cls, search_term):
+        profiles = cls.objects.filter(user__username__icontains=search_term)
+        return profiles
+
+    @property
+    def image_url(self):
+        if self.profile_pic and hasattr(self.profile_pic, 'url'):
+            return self.profile_pic.url
+
+    def __str__(self):
+        return self.user.username
+
 class Image(models.Model):
     image=models.ImageField(upload_to='picture/', )
     name = models.CharField(max_length=40)
@@ -137,4 +174,54 @@ class Image(models.Model):
     def update_description(cls, id):
         pictures = cls.objects.filter(id=id).update(id=id)
         return pictures
+    
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+
+    )
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='reviews')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="reviews", null=True, blank=True)
+    comment = models.TextField()
+    design_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+    usability_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+    content_rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+
+    def save_comment(self):
+        self.save()
+
+    def get_comment(self, id):
+        comments = Review.objects.filter(image_id =id)
+        return comments
+
+    def __str__(self):
+        return self.comment
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE,)
+    value = models.IntegerField(default=True, null=True, blank=True)
+
+    def save_like(self):
+        self.save()
+
+    def __str__(self):
+        return str(self.user) + ':' + str(self.image) + ':' + str(self.value)
+
+    class Meta:
+        unique_together = ("user", "image", "value")
 
--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,9 +1,116 @@
 from django.shortcuts import render
+from .models import Profile,Project
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from .models import Profile,Project
+from .forms import NewImageForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
-    all_post=Post.objects.all()
+    all_project=Project.objects.all()
     
-    return render('index.html',{'all_post':all_post})
+    return render(request,'index.html',{'all_project':all_project})
+
+def signin(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have successfuly loged in")
+            return redirect ("/")
+    return render(request,'login.html')
+
+def register(request):
+    if request.method == "POST":
+        username=request.POST["username"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+
+        if password1 !=password2:
+            messages.error(request,'Password do not match')
+            return render('/register')
+        new_user=User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password1,
+        ) 
+        new_user.save() 
+        return render (request,'login.html')
+    return render(request,'register.html')
+
+def profile(request):
+    user=request.user
+    my_profile=Profile.objects.get(user=user)
+    return render(request,"profile.html",{'my_profile':my_profile,"user":user})
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = UpdatebioForm(request.POST, request.FILES, instance=current_user.profile)
+        print(form.is_valid())
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('homePage')
+
+    else:
+        form = UpdatebioForm()
+    return render(request, 'registration/edit_profile.html', {"form": form})
+
+def signout(request):
+    logout(request)
+    messages.success(request,"You have logged out, we will be glad to have you back again")
+    return redirect ("login")
+
+@login_required(login_url='/accounts/login/')
+def addpost(request):
+    if request.method=="POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post= form.save(commit=False)
+            post.user=request.user
+            post.save()
+            return redirect('/')
+
+    else:
+        form=PostForm()
+    try:
+        posts=Post.objects.all() 
+        posts=posts[::-1] 
+    except Post.DoesNotExist:
+        posts=None
+    return render(request,'newpost.html',{"form":form,"posts":posts}) 
+
+@login_required(login_url='/accounts/login/')
+def postproject(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user=request.user
+            project.save()
+        return redirect('/')
+    else:
+        form = PostForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'newpost.html', context)
+
+
