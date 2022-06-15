6ca4a3ac6ebe1ec70dfa78b83160c0e59504c520
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-15 14:22:23 +0300

    deploy

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -132,7 +132,7 @@ WSGI_APPLICATION = 'awardsproject.wsgi.application'
 
 DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
